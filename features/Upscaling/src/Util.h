e11c4d406ca9517a3e768babe83a43a37576ec49
Kuzey Gok <kuzeygokk@outlook.com>
2026-05-19 02:09:22 -0700

    style(upscaling): match a_-prefix parameter naming convention

--- features/Upscaling/src/Util.h
+++ features/Upscaling/src/Util.h
@@ -62,7 +62,7 @@ namespace Util
 		return rtm->dynamicHeightRatio;
 	}
 
-	ID3D11DeviceChild* CompileShader(const wchar_t* FilePath, const std::vector<std::pair<const char*, const char*>>& Defines, const char* ProgramType, const char* Program = "main");
+	ID3D11DeviceChild* CompileShader(const wchar_t* a_filePath, const std::vector<std::pair<const char*, const char*>>& a_defines, const char* a_programType, const char* a_program = "main");
 }
 
 }
